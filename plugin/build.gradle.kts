b74b89c1b6e6af914b6b80ef13a269a9e38c0ff0
Familstorm <ngochoacth53@gmail.com>
2024-07-30 10:37:53 +0700

    #update config plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 }
 
 // TODO: Update value to your plugin's name.
-val pluginName = "GodotAndroidPluginTemplate"
+val pluginName = "GodotSqliteCSharp"
 
 // TODO: Update value to match your plugin's package name.
-val pluginPackageName = "org.godotengine.plugin.android.template"
+val pluginPackageName = "org.familstorm.plugin.android.sqlite"
 
 android {
     namespace = pluginPackageName
